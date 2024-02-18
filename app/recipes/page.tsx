c0a5e51574c2fce80f5e1e6e0a68ebb40fa20f22
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2024-02-18 12:27:30 +0100

    first commit

---
+++ app/recipes/page.tsx
@@ -0,0 +1,9 @@
+const recipes = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Test</div>
+    </main>
+  );
+};
+
+export default recipes;
